269499ed18ae55f750fd26f139ae1d1eb34928e0
Albert Zhang <zjw0602@foxmail.com>
2016-03-01 15:49:06 +0800

    add learn template

--- learn/views.py
+++ learn/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    List = map(str, range(100))
+    return render(request, 'home.html', {'List': List})
