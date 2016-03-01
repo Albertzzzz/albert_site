cd3628deeda82acb969f26e1cedda85b909ea774
Albert Zhang <zjw0602@foxmail.com>
2016-03-01 14:54:01 +0800

    add learn app

--- albert_site/urls.py
+++ albert_site/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^home/$', include('learn.urls')),
     url(r'^add/', include('calc.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
